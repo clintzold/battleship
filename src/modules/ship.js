ca7374ff53172ab5c8f4ee4e87029ea1a2872bbf
clint zold <214383878+clintzold@users.noreply.github.com>
2026-08-16 02:55:10 -0600

    create ship class and relevant tests

---
+++ src/modules/ship.js
@@ -0,0 +1,18 @@
+// ./src/modules/ship.js
+
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits++
+  }
+
+  isSunk() {
+    return this.hits === this.length
+  }
+
+
+}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,52 @@
+// ./src/tests/ship.test.js
+import Ship from '../modules/ship.js'
+
+test('creates a new Ship instance', () => {
+  const ship = new Ship(3)
+  expect(ship).toBeInstanceOf(Ship)
+})
+
+describe('Ship: Properties', () => {
+  const ship = new Ship(3)
+
+  test('has a length property', () => {
+    expect(ship).toHaveProperty('length')
+  })
+
+  test('has a hits property', () => {
+    expect(ship).toHaveProperty('hits')
+  })
+})
+
+describe('Ship: Functions', () => {
+  let ship
+
+  beforeEach(() => {
+    ship = new Ship(3)
+  })
+
+  test('has a hit() function that increments the hits property', () => {
+    ship.hit()
+    expect(ship.hits).toBe(1)
+    ship.hit()
+    expect(ship.hits).toBe(2)
+    ship.hit()
+    expect(ship.hits).toBe(3)
+  })
+
+  test('has a isSunk() function that returns a boolean', () => {
+    expect(typeof ship.isSunk()).toBe('boolean')
+  })
+
+  test('isSunk() returns false if hits are less than length', () => {
+    ship.hit()
+    expect(ship.isSunk()).toBe(false)
+  })
+
+  test('isSunk() returns true if hits are equal to length', () => {
+    ship.hit()
+    ship.hit()
+    ship.hit()
+    expect(ship.isSunk()).toBe(true)
+  })
+})
